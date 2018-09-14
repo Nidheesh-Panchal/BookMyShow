d3600211d1054856185a4c8e6a251e52d2f69668
Nidheesh Panchal <33429560+Nidheesh-Panchal@users.noreply.github.com>
2018-09-15 00:16:09 +0530

    Add files via upload

---
+++ Dump20180915.sql
@@ -0,0 +1,198 @@
+CREATE DATABASE  IF NOT EXISTS `bms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `bms`;
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bms
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cinemahall`
+--
+
+DROP TABLE IF EXISTS `cinemahall`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cinemahall` (
+  `challid` int(11) NOT NULL,
+  `chname` varchar(45) NOT NULL,
+  `chlocation` varchar(45) NOT NULL,
+  PRIMARY KEY (`challid`),
+  UNIQUE KEY `chname_UNIQUE` (`chname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cinemahall`
+--
+
+LOCK TABLES `cinemahall` WRITE;
+/*!40000 ALTER TABLE `cinemahall` DISABLE KEYS */;
+INSERT INTO `cinemahall` VALUES (1,'Raj Mandir','MI Road');
+/*!40000 ALTER TABLE `cinemahall` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `customer` (
+  `username` varchar(25) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `emailid` varchar(50) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `idusername_UNIQUE` (`username`),
+  UNIQUE KEY `emailid_UNIQUE` (`emailid`),
+  UNIQUE KEY `firstname_UNIQUE` (`firstname`),
+  UNIQUE KEY `lastname_UNIQUE` (`lastname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES ('Akshita','a','2016ucp1023@mnit.ac.in','Akshita','agravaaals'),('Nidheesh','n','2016ucp1008@mnit.ac.in','Nidheesh','Panchal'),('Vandita','v','2016ucp1004@mnit.ac.in','Vandita','Goyal');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `movie` (
+  `movieid` int(11) NOT NULL,
+  `mname` varchar(45) NOT NULL,
+  `mdescription` text NOT NULL,
+  `genre` varchar(45) NOT NULL,
+  `duration` int(5) NOT NULL,
+  PRIMARY KEY (`movieid`),
+  UNIQUE KEY `mname_UNIQUE` (`mname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `screening`
+--
+
+DROP TABLE IF EXISTS `screening`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `screening` (
+  `screeningid` int(11) NOT NULL,
+  `movieid` int(11) NOT NULL,
+  `theatreid` int(11) NOT NULL,
+  `challid` int(11) NOT NULL,
+  `time` int(4) NOT NULL,
+  `date` date NOT NULL,
+  `noseatbooked` int(11) NOT NULL,
+  `cost` int(11) NOT NULL,
+  PRIMARY KEY (`theatreid`,`challid`,`time`,`date`),
+  KEY `challid_idx` (`challid`,`theatreid`),
+  KEY `movieidfk_idx` (`movieid`),
+  CONSTRAINT `movieid` FOREIGN KEY (`movieid`) REFERENCES `movie` (`movieid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `theatreidfk` FOREIGN KEY (`theatreid`, `challid`) REFERENCES `theatre` (`theatreid`, `challid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `screening`
+--
+
+LOCK TABLES `screening` WRITE;
+/*!40000 ALTER TABLE `screening` DISABLE KEYS */;
+/*!40000 ALTER TABLE `screening` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `theatre`
+--
+
+DROP TABLE IF EXISTS `theatre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `theatre` (
+  `theatreid` int(11) NOT NULL,
+  `challid` int(11) NOT NULL,
+  `noseats` int(11) NOT NULL DEFAULT '49',
+  PRIMARY KEY (`theatreid`,`challid`),
+  KEY `challid_idx` (`challid`),
+  CONSTRAINT `challid` FOREIGN KEY (`challid`) REFERENCES `cinemahall` (`challid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `theatre`
+--
+
+LOCK TABLES `theatre` WRITE;
+/*!40000 ALTER TABLE `theatre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `theatre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `ticket` (
+  `username` varchar(25) NOT NULL,
+  `screeningid` int(11) NOT NULL,
+  `seatno` int(11) NOT NULL,
+  PRIMARY KEY (`screeningid`,`seatno`),
+  KEY `usernamefk_idx` (`username`),
+  CONSTRAINT `usernamefk` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-15  0:14:46
